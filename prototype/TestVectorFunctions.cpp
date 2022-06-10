4341ecf7af39b208cc437e32b305e534211981a9
Dave Amiana <amianism25@gmail.com>
2022-06-11 02:04:31 +0800

    updated prototype

--- prototype/TestVectorFunctions.cpp
+++ prototype/TestVectorFunctions.cpp
@@ -1,16 +1,11 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-#include <concepts>
-#include <type_traits>
 #include <algorithm>
 #include <functional>
-#include <tuple>
 
-/*
- * todo
- * - variadic functions
- * */
+template<typename T>
+using VectorFunction = std::function<std::vector<T>(std::vector<T>)>;
 
 template <typename T>
 auto operator<<(std::ostream& os, 
@@ -50,22 +45,6 @@ auto tan(const std::vector<T>& vec) -> std::vector<T> {
 	);
 	return result;
 }
-// template <typename R, typename ... P> 
-// auto apply_function(const std::function<auto (P...) -> R>& f) -> void {
-//	apply_function(std::function<auto (P...) -> R>(f));
-// }
-
-template <typename T>
-auto f(const std::function<T(T...)>& fn, 
-		const std::tuple<T>& tp) -> T {
-	
-	return std::apply(fn, tp);
-}
-
-auto dd(const std::vector<double>& t1,
-		const std::vector<double>& t2) -> std::vector<double> {
-	return t1;
-}
 
 template <typename T> 
 auto vector_function(const std::vector<std::function<std::vector<T>(std::vector<T>)>>& fn,
@@ -77,13 +56,11 @@ auto vector_function(const std::vector<std::function<std::vector<T>(std::vector<
 	return results;
 } 
 
-typedef std::function<std::vector<double>(std::vector<double>)> Function;
-
 auto main(void) -> int {
 	std::vector<double> v {1.3,2.4,5.6};
-	Function f = sin<>;
-	Function g = cos<>;
-	auto fn  = std::vector<Function>{f, g};
+	VectorFunction<double> f = sin<>;
+	VectorFunction<double> g = cos<>;
+	auto fn  = std::vector<VectorFunction<double>>{f, g};
 
-	std::cout << vector_function(fn, std::vector<double>{1.2,2.3,2.5});
+	std::cout << vector_function(fn,v);
 }
