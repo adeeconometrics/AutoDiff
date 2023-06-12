2eabdda29b9ae8f6d5da8a8822d0e48c8631fe06
ddamiana <amianism25@gmail.com>
2023-06-12 16:09:20 +0800

    restructured

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include "../include/fsymbol.hpp"
 #include "../include/rsymbol.hpp"
 
 #include <iostream>
@@ -5,15 +6,25 @@
 using namespace ad;
 using std::cout;
 
+// auto tf(double x, double y) -> FSym {
+//   FSym a {}
+// }
+
 auto main() -> int {
 
   RSym a{1.1};
   RSym b{0.5};
 
+  FSym fa{1.1};
+  FSym fb{0.5};
+
+  const auto f = fb * fa + fa * fb;
+
   auto expr = a / b + b * a;
   auto df_expr = gradient(expr);
 
-  cout << df_expr[a] << '\n' << df_expr[b];
+  cout << df_expr[a] << '\n' << df_expr[b] << "\n\n";
+  cout << f.dot();
 
   return 0;
 }
