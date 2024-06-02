ded8bd6e7a0c7d902b5603cb649429dd6d83142a
Dave Amiana <amianism25@gmail.com>
2024-06-02 22:57:31 +0800

    tested for rsym arithmetic

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -3,6 +3,7 @@
 #include "../include/fsymbol.hpp"
 #include "../include/rsymbol.hpp"
 
+#include <map>
 /**
  * @brief Include testing for partial derivatives of FSym
  *
@@ -46,4 +47,52 @@ TEST(FSymbol, DivScalar) {
 
   EXPECT_DOUBLE_EQ(c.value(), 2.2);
   EXPECT_DOUBLE_EQ(c.dot(), 1 / 0.5);
+}
+
+TEST(RSymbol, AddScalar) {
+  ad::RSym a{1.1};
+  ad::RSym b{0.5};
+
+  auto c = a + b;
+  const auto df_c = ad::gradient(c);
+
+  EXPECT_EQ(c.value(), 1.6);
+  EXPECT_EQ(df_c.at(a), 1.0);
+  EXPECT_EQ(df_c.at(b), 1.0);
+}
+
+TEST(RSymbol, SubScalar) {
+  ad::RSym a{1.1};
+  ad::RSym b{0.5};
+
+  auto c = a - b;
+  const auto df_c = ad::gradient(c);
+
+  EXPECT_DOUBLE_EQ(c.value(), 0.6);
+  EXPECT_DOUBLE_EQ(df_c.at(a), 1.0);
+  EXPECT_DOUBLE_EQ(df_c.at(b), -1.0);
+}
+
+TEST(RSymbol, MulScalar) {
+  ad::RSym a{1.1};
+  ad::RSym b{0.5};
+
+  auto c = a * b;
+  const auto df_c = ad::gradient(c);
+
+  EXPECT_DOUBLE_EQ(c.value(), 0.55);
+  EXPECT_DOUBLE_EQ(df_c.at(a), 0.5);
+  EXPECT_DOUBLE_EQ(df_c.at(b), 1.1);
+}
+
+TEST(RSymbol, DivScalar) {
+  ad::RSym a{1.1};
+  ad::RSym b{0.5};
+
+  auto c = a / b;
+  const auto df_c = ad::gradient(c);
+
+  EXPECT_DOUBLE_EQ(c.value(), 2.2);
+  EXPECT_DOUBLE_EQ(df_c.at(a), 1 / 0.5);
+  EXPECT_DOUBLE_EQ(df_c.at(b), -1.1 / 0.5 / 0.5);
 }
